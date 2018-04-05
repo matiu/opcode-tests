4e0fb69656b58fe69fc33d1153f9fd12f122fc2c
Matias Alejo Garcia <ematiu@gmail.com>
2018-04-05 03:19:35 -0300

    batch tests

--- stress-opcodes.sh
+++ stress-opcodes.sh
@@ -2,59 +2,94 @@
 
 CLI="$HOME/dev/bitcoin-abc/build/src/bitcoin-cli -regtest "
 CLITX="$HOME/dev/bitcoin-abc/build/src/bitcoin-tx -regtest "
-SCRIPT='1 1 XOR 1 XOR 1 XOR 1 XOR'
+#SCRIPT='0 BIN2NUM BIN2NUM BIN2NUM BIN2NUM BIN2NUM BIN2NUM BIN2NUM BIN2NUM BIN2NUM BIN2NUM BIN2NUM BIN2NUM BIN2NUM BIN2NUM BIN2NUM BIN2NUM BIN2NUM BIN2NUM 0 EQUAL'
+#SCRIPT='1 1 XOR 1 XOR 1 XOR 1 XOR'
+SCRIPT='1'
 FEE=0.0001
-COUNT=100
+COUNT=1000
 
 ECHO="echo "
 
 
+$ECHO "TEST CASE DESCRIPTON"
+$ECHO "  Script:$SCRIPT"
+$ECHO "  (nr opcodes):`echo $SCRIPT | wc -w`"
+$ECHO "  NR OF TXs: $COUNT"
 
-$CLI generate 100 >/dev/null || exit
+$ECHO " == Mining"
+$CLI generate $COUNT >/dev/null || exit
 
-function setUnspent () {
-    UNSPENT=`$CLI  listunspent | jq '.|=sort_by(.amount)|.[-1]'`
-    OUTPOINT=`echo $UNSPENT | jq .txid | sed -e 's/^"//' -e 's/"$//'`:0
-    VALUE=`echo $UNSPENT | jq .amount | sed -e 's/^"//' -e 's/"$//'`
-    VALUE=`echo $VALUE - $FEE | bc`
+function getUnspent () {
+    $ECHO " == Getting UTXOs"
+    ALL_UTXOS=`$CLI  listunspent | jq ".|=sort_by(.amount)|reverse|.[0:$COUNT]"`
+
+    COUNTER=0
+    while [  $COUNTER -lt $COUNT ]; do
+        INDEX=$((1+COUNTER))
+        UTXO=`echo $ALL_UTXOS| jq ".[-$INDEX]"`
+
+        OUTPOINT=`echo $UTXO | jq .txid | sed -e 's/^"//' -e 's/"$//'`:0
+        VALUE=`echo $UTXO | jq .amount | sed -e 's/^"//' -e 's/"$//'`
+        VALUE=`echo $VALUE - $FEE | bc`
+
+
+        UNSPENT="$UNSPENT $OUTPOINT|$VALUE"
+
+        let COUNTER=COUNTER+1 
+    done
 }
 
-function createTx () {
-    $ECHO ""
-    $ECHO "## Creating TX:"
-    $ECHO "  Value:$VALUE"
-    $ECHO "  Script:$SCRIPT"
-    $ECHO "  UTXO: $OUTPOINT"
-    TX1=`$CLITX -create in=$OUTPOINT outscript=$VALUE:"$SCRIPT"`
-    TX=`$CLI signrawtransaction $TX1| jq .hex  | sed -e 's/^"//' -e 's/"$//'`
+function createTxs () {
+    $ECHO " == Creating TXs"
+    for UTXO in $UNSPENT; do
+        O=`echo $UTXO| cut -d "|" -f 1`;
+        V=`echo $UTXO| cut -d "|" -f 2`;
+
+        TX1=`$CLITX -create in=$O outscript=$V:"$SCRIPT"`
+        TX=`$CLI signrawtransaction $TX1| jq .hex  | sed -e 's/^"//' -e 's/"$//'`
+
+        TXS="$TXS $TX|$V"
+    done
+}
+
+function sendTxs () {
+    $ECHO " == Sending TXs"
+    COUNTER=0
+    for TXVALUE in $TXS; do
+        TX=`echo $TXVALUE| cut -d "|" -f 1`;
+        V=`echo $TXVALUE| cut -d "|" -f 2`;
+
+        TXID=`$CLI sendrawtransaction $TX`
+        TXIDS="$TXIDS $TXID|$V"
+    done
 }
 
 
-function sendTx () {
-    TXID=`$CLI sendrawtransaction $TX`
+function createSpendTxs () {
+    $ECHO " == Creating Spend TXs"
+    for TXIDVALUE in $TXIDS ; do
+        TXID=`echo $TXIDVALUE| cut -d "|" -f 1`;
+        V=`echo $TXIDVALUE| cut -d "|" -f 2`;
+        VALUE=`echo "$V - $FEE" | bc`
+        TX=`$CLITX -create in=$TXID:0 outscript=$VALUE:"$SCRIPT"`
+        STXS="$STXS $TX"
+    done
 }
 
+function sendSpendTxs () {
+    $ECHO " ## Sending Spend TXs, USING <SCRIPT> ##"
+    COUNTER=0
+    for TX in $STXS; do
+        TXID=`$CLI sendrawtransaction $TX`
+    done
+}
 
-COUNTER=0
-while [  $COUNTER -lt $COUNT ]; do
-    setUnspent
-    createTx
-    #echo $TX
-    #$CLI decoderawtransaction $TX
+getUnspent
+createTxs
+time sendTxs
 
-    sendTx
-    echo "  => $TXID"
-
-    #spend it
-
-    VALUE=`echo $VALUE - $FEE | bc`
-    TX=`$CLITX -create in=$TXID:0 outscript=$VALUE:"$SCRIPT"`
-    sendTx
-    $ECHO "    ^ SPENT AT:$TXID"
-
-
-    let COUNTER=COUNTER+1 
-done
+createSpendTxs
+time sendSpendTxs
 
 
 
